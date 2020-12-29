8626e4b3f4dacd96cac28f346da37191b561b71d
loay <loaymannaa@outlook.com>
2020-12-29 22:49:37 +0100

    Fix shopping cart error and update details btn

--- DotNetCoreRazor/Pages/Customer/Home/Index.cshtml.cs
+++ DotNetCoreRazor/Pages/Customer/Home/Index.cshtml.cs
@@ -25,17 +25,20 @@ namespace DotNetCoreRazor.Pages.Customer.Home
 
         public void OnGet()
         {
-            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
-            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            
+                var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
-            if (claim != null)
-            {
-                int shoppingCartCount = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).ToList().Count;
-                HttpContext.Session.SetInt32(SD.ShoppingCart, shoppingCartCount);
-            }
+                if (claim != null)
+                {
+                    int shoppingCartCount = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).ToList().Count;
+                    HttpContext.Session.SetInt32(SD.ShoppingCart, shoppingCartCount);
+                }
 
-            MenuItemList = _unitOfWork.MenuItem.GetAll(null, null, "Category,FoodType");
-            CategoryList = _unitOfWork.Category.GetAll(null, q => q.OrderBy(c => c.DisplayOrder), null);
+                MenuItemList = _unitOfWork.MenuItem.GetAll(null, null, "Category,FoodType");
+                CategoryList = _unitOfWork.Category.GetAll(null, q => q.OrderBy(c => c.DisplayOrder), null);
+            
+           
         }
     }
 }
