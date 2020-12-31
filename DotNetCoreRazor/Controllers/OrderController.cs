3c54b1d33b739bb1443d6705a4ee0f800bc178c5
loay <loaymannaa@outlook.com>
2020-12-31 13:26:12 +0100

    Create OrderController add orderDetails page and ManageOrder page

---
+++ DotNetCoreRazor/Controllers/OrderController.cs
@@ -0,0 +1,75 @@
+﻿using DataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Model.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Utility;
+
+namespace DotNetCoreRazor.Controllers
+{
+    public class OrderController : Controller 
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get(string status = null)
+        {
+            List<OrderDetailsViewModel> orderListVM = new List<OrderDetailsViewModel>();
+
+            IEnumerable<OrderHeader> OrderHeaderList;
+
+            if (User.IsInRole(SD.CustomerRole))
+            {
+                //retrieve all orders for that customer
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                OrderHeaderList = _unitOfWork.OrderHeader.GetAll(u => u.UserId == claim.Value, null, "ApplicationUser");
+            }
+            else
+            {
+                OrderHeaderList = _unitOfWork.OrderHeader.GetAll(null, null, "ApplicationUser");
+            }
+
+            if (status == "cancelled")
+            {
+                OrderHeaderList = OrderHeaderList.Where(o => o.Status == SD.StatusCancelled || o.Status == SD.StatusRefunded || o.Status == SD.PaymentStatusRejected);
+            }
+            else
+            {
+                if (status == "completed")
+                {
+                    OrderHeaderList = OrderHeaderList.Where(o => o.Status == SD.StatusCompleted);
+                }
+                else
+                {
+                    OrderHeaderList = OrderHeaderList.Where(o => o.Status == SD.StatusReady || o.Status == SD.StatusInProcess || o.Status == SD.StatusSubmitted || o.Status == SD.PaymentStatusPending);
+                }
+            }
+
+            foreach (OrderHeader item in OrderHeaderList)
+            {
+                OrderDetailsViewModel individual = new OrderDetailsViewModel
+                {
+                    OrderHeader = item,
+                    OrderDetails = _unitOfWork.OrderDetails.GetAll(o => o.OrderId == item.Id).ToList()
+                };
+                orderListVM.Add(individual);
+            }
+
+            return Json(new { data = orderListVM });
+        }
+
+    }
+}
---
+++ Model/ViewModel/OrderDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.ViewModel
+{
+  public  class OrderDetailsViewModel
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public List<OrderDetails> OrderDetails { get; set; }
+    }
+}
